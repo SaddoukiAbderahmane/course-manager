3dd00fc15c7226d18e450aaccefda4e7c91fdaeb
SaddoukiAbderahmane <156526734+SaddoukiAbderahmane@users.noreply.github.com>
2024-01-14 00:30:44 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(coursemanagement.Startup))]
+
+namespace coursemanagement
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on  configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login")
+            });
+        }
+    }
+}
